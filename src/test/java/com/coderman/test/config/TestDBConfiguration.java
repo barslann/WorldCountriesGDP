54f1fc1f5e298be55501094a21be6f3c169869bd
coderman <barslann58@gmail.com>
2021-03-30 15:16:41 -0700

    setting up the test environment

---
+++ src/test/java/com/coderman/test/config/TestDBConfiguration.java
@@ -0,0 +1,33 @@
+package com.coderman.test.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class TestDBConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.H2)
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .addScript("h2_world.sql")
+                .build();
+    }
+
+    @Bean("namedParamJdbcTemplate")
+    public NamedParameterJdbcTemplate namedParamJdbcTemplate() {
+        NamedParameterJdbcTemplate namedParamJdbcTemplate =
+                new NamedParameterJdbcTemplate(dataSource());
+        System.out.println("********************************** ->"+namedParamJdbcTemplate);
+        return namedParamJdbcTemplate;
+    }
+}
