fb08b56fc98b4123622ffc4198e10113d5db3170
coderman <barslann58@gmail.com>
2021-03-30 17:27:13 -0700

    defining the RowMapper

---
+++ src/main/java/com/coderman/worldgdp/dao/mapper/CityRowMapper.java
@@ -0,0 +1,22 @@
+package com.coderman.worldgdp.dao.mapper;
+
+import com.coderman.worldgdp.model.City;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CityRowMapper implements RowMapper<City> {
+
+    public City mapRow(ResultSet rs, int rowNum) throws SQLException {
+        City city = new City();
+        city.setCountryCode(rs.getString("country_code"));
+        city.setDistrict(rs.getString("district"));
+        city.setId(rs.getLong("id"));
+        city.setName(rs.getString("name"));
+        city.setPopulation(rs.getLong("population"));
+        return city;
+
+    }
+
+}
